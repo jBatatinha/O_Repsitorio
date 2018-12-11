b12ae73f9d465ffd2f99e094c1cb2f1bc402a823
bsussel <brunosussel@gmail.com>
2018-12-11 19:04:05 -0200

    Adicionadas algumas funções de motor
    Adicionadas funções motorDireitaParar(), motorEsquerdaParar(), motorDireitaFrente() e motorEsquerdaFrente(). Agora a função seguirLinha() pode funcionar adequadamente

--- ganguedosgangueiros/motores.ino
+++ ganguedosgangueiros/motores.ino
@@ -11,14 +11,30 @@ void seguirFrente(){
    motorDireitaFrente(); 
 }
 void motorDireitaParar(){
-  
+  int in1;
+  int in2;
+
+  digitalWrite (in1, HIGH);
+  digitalWrite (in2, HIGH);
 }
 void motorDireitaFrente(){
-  
+  int in1;
+  int in2;
+
+  digitalWrite (in1, LOW);
+  digitalWrite (in2, HIGH);
 }
 void motorEsquerdaParar(){
-  
+  int in3;
+  int in4;
+
+  digitalWrite (in3, HIGH);
+  digitalWrite (in4, HIGH);
 }
 void motorEsquerdaFrente(){
-  
+  int in3;
+  int in4;
+
+  digitalWrite (in3, LOW);
+  digitalWrite (in4, HIGH);
 }
