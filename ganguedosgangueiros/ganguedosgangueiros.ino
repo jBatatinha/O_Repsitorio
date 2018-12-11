c6a16d5476b9af60d91f15066e7ecdf7704cc3d4
bsussel <brunosussel@gmail.com>
2018-12-11 19:35:31 -0200

    Início progamação desvio de obstáculos
    Adicionado função desviarObstaculo ()

--- ganguedosgangueiros/ganguedosgangueiros.ino
+++ ganguedosgangueiros/ganguedosgangueiros.ino
@@ -9,6 +9,7 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
+  desviarObstaculo();
   seguirLinha ();
 }
 
--- ganguedosgangueiros/progamacoesPrincipais.ino
+++ ganguedosgangueiros/progamacoesPrincipais.ino
@@ -1,3 +1,4 @@
+//Seguir Linha
 void seguirLinha () {
   if (pretoDireita()) {
     virarDireita();
@@ -40,5 +41,38 @@ bool pretoEsquerda(){
   else{
     return false;
   }
+}
+
+//Desviar de obstáculos
+void desviarObstaculo(){
+  int delayGiro=100;
+  int delay1=100;
+  int delay2=100;
+  if (temObstaculo ()){
+    girarEsquerda();
+    delay (delayGiro);
+    seguirFrente();
+    delay (delay1);
+    girarDireita();
+    delay (delayGiro);
+    seguirFrente ();
+    delay (delay2);
+    girarDireita ();
+    delay (delayGiro);
+    seguirFrente();
+    delay (delay1);
+    girarEsquerda();
+    delay (delayGiro);
+  }
+}
+
+void girarEsquerda (){
+  
+}
+void girarDireita (){
+  
+}
+bool temObstaculo (){
+  
 }
 
