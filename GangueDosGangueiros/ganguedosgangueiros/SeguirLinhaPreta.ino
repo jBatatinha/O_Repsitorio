05ffaafcdc4ad9ebc5f247d4d1a0cf5fbb0520ea
bsussel <42359091+bsussel@users.noreply.github.com>
2018-12-11 01:20:17 -0200

    Correção inicío da progamação
    Declarada outras funções que estavam dentro de seguirLinha () para progamação poder ser compilada

---
+++ GangueDosGangueiros/ganguedosgangueiros/SeguirLinhaPreta.ino
@@ -0,0 +1,27 @@
+void seguirLinha () {
+  if (pretoDireita()) {
+    virarDireita();
+  }
+  else if (pretoEsquerda()) {
+    virarEsquerda();
+  }
+  else {
+    seguirFrente();
+  }
+}
+
+bool pretoDireita(){
+  return true;
+}
+bool pretoEsquerda(){
+  return false;
+}
+void virarDireita(){
+}
+void virarEsquerda(){
+  
+}
+void seguirFrente(){
+
+}
+
