bf99be394b67984c2cfd964abfcdb133cb07cffb
bsussel <brunosussel@gmail.com>
2018-12-11 19:19:05 -0200

    Reorganização da progamação 2
    Separei as funções do sensor de cor do lado direito das do lado esquerdo

---
+++ ganguedosgangueiros/sensorCorDireita.ino
@@ -0,0 +1,23 @@
+void ligarSensorCorDireita(){
+  int s0;
+  int s1;
+
+  digitalWrite (s0, HIGH);
+  digitalWrite (s1, HIGH);
+}
+void filtroPretoDireita(){
+  int s2;
+  int s3;
+
+  digitalWrite (s2, HIGH);
+  digitalWrite (s3, LOW);
+}
+int leituraDireita(){
+  int out;
+  if (digitalRead (out) == HIGH){
+    return pulseIn (out, LOW);
+  }
+  else {
+    return pulseIn (out, HIGH);
+  }
+}
---
+++ ganguedosgangueiros/sensorCorEsquerda.ino
@@ -0,0 +1,24 @@
+void ligarSensorCorEsquerda(){
+  int s0;
+  int s1;
+
+  digitalWrite (s0, HIGH);
+  digitalWrite (s1, HIGH);
+}
+void filtroPretoEsquerda(){
+  int s2;
+  int s3;
+
+  digitalWrite (s2, HIGH);
+  digitalWrite (s3, LOW);
+}
+int leituraEsquerda(){
+    int out;
+  if (digitalRead(out) == HIGH){
+    return pulseIn (out, LOW);
+  }
+  else {
+    return pulseIn (out, HIGH);
+  }
+}
+
